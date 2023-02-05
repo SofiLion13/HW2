e34a2c151495db98621a2a985bfca0160788aaeb
Sofia <dragsy13@bk.ru>
2023-02-05 22:33:06 +0300

    Add new task

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = GetNumber("Введите трехзначное число: ");
+
+// 2. Оставить 2 цифру числа
+int GetTheSecondNumber(int number)
+{
+    int secondDigit = number / 10 % 10;
+    return secondDigit;
+}
+int result = GetTheSecondNumber(number);
+
+Console.WriteLine($"Было: {number} Стало: {result}");
